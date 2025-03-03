b605b02959caa07579aeba2fe1209f81320d537c
Risholmaayo <142151088+Risholmaayo@users.noreply.github.com>
2025-03-03 14:31:01 +0700

    Update BinaryChecker.py

--- BinaryChecker.py
+++ BinaryChecker.py
@@ -1,1 +1,15 @@
+def is_binary(s):
+    # Periksa setiap karakter dalam string
+    for char in s:
+        if char not in {'0', '1'}:
+            return False
+    return True
 
+# Ambil input 
+user_input = input("Input: ")
+
+# Periksa input
+if is_binary(user_input):
+    print(f"'{user_input}' adalah bilangan biner.")
+else:
+    print(f"'{user_input}' bukan bilangan biner.")
